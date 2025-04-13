78bf2e16aa8514f6748a7ee71287ae5127d6235a
ShekarShivaji <shivajiss795@gmail.com>
2025-04-13 22:50:47 +0530

    Added frontend and backend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import Signup from "./components/Signup"
 import Login from "./components/Login"
 import Category from "./components/Category"
-import {BrowserRouter , Route,Routes } from "react-router-dom"
+import {HashRouter  , Route,Routes } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div>
-       <BrowserRouter>
+       <HashRouter >
       <Routes>
         <Route path="/signup"  element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Category />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter >
     </div>
   )
 }
